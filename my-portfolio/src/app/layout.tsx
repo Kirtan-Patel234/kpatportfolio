b8447e5687fb35a9b51f70e6cb2c2894eff9f1e7
Kirtan-Patel234 <patel.kirtan2004@outlook.com>
2025-09-18 02:27:24 -0500

    added web page analytics

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="font-sans">
-        <main>{children}</main>
+        <main>{children}
+        <Analytics />
+        </main>
       </body>
     </html>
   );
